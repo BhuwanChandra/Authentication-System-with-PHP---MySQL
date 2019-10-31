03599191472c918128744ab3830c576a86c24194
BhuwanChandra <bhuwan7779@gmail.com>
2019-10-31 23:05:53 +0530

    added home page styling

--- home.php
+++ home.php
@@ -15,12 +15,26 @@ if(!isset($_SESSION['currentUser'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>HOME</title>
+    <style>
+    h2{
+        color: blueviolet;
+        margin-top: 25px;
+        margin-bottom: 25px;
+    }
+    a{
+        text-decoration: none;
+        color: green;
+        font-size: 30px;
+        margin: 25px;
+        font-weight: 600;
+    }
+    </style>
 </head>
 <body>
-<center>
-    <h2>Welcome <?php echo $_SESSION['currentUser']; ?></h2>
-</center>
+    <center>
+        <h2>Welcome <?php echo $_SESSION['currentUser']; ?></h2>
+    </center>
+    <br>
     <a href="logout.php">LOGOUT</a>
-
 </body>
 </html>
